8e8f211a130e8ecba1c5884794ee297ec0c27c25
tony <idokoanthony86@yahoo.com>
2021-03-22 20:29:55 +0100

    modified settings by including mimetype

--- photo/models.py
+++ photo/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
 
 class Picture(models.Model):
     category = models.ForeignKey("Category", on_delete = models.CASCADE)
-    image = models.ImageField(upload_to = "media/pictures")
+    image = models.ImageField(upload_to = "pictures")
     description = models.CharField(max_length = 2000)
     date_added = models.DateTimeField(default = datetime.now)
+
+    def __str__(self):
+        return self.description
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Category, Picture
 # Create your views here.
+
+def home_view(request):
+    categories = Category.objects.all()
+
+    return render(request,"home.html",{"categories":categories})
