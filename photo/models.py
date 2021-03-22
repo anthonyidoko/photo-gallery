6698f1469222a9ab4e80c0d501326b5870dd895e
tony <idokoanthony86@yahoo.com>
2021-03-22 16:04:15 +0100

    created model

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 1000)
+
+    def __str__(self):
+        return self.category
+        
+class Picture(models.Model):
+    category = models.ForeingKey("Category", on_delete = models.CASCADE)
+    image = models.ImageField(upload_to = "pictures")
+    date_added = models.DateTimeField(default = datetime.now)
