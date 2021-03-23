cb2a1194ddbe0502b276c27d3c11efb9f37b5217
tony <idokoanthony86@yahoo.com>
2021-03-23 13:53:42 +0100

    updated settings.py

--- photo/urls.py
+++ photo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, photos_view
 
 
 urlpatterns = [
     path('', home_view, name = "home"),
+    path("photos/<int:pk>/", photos_view,name = "photos")
 ]
--- photo/views.py
+++ photo/views.py
@@ -6,3 +6,14 @@ def home_view(request):
     categories = Category.objects.all()
 
     return render(request,"home.html",{"categories":categories})
+
+
+def photos_view(request,pk):
+    category = Category.objects.get(id=pk)
+    pictures = Picture.objects.filter(category = category)
+    context = {
+        "category":category,
+        'pictures':pictures
+    }
+
+    return render(request, "photos.html", context)
